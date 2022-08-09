45b3490e3d0749788615d594f87cab539361de85
Wandile <nondabulaw@sentech.com>
2022-08-09 14:43:49 +0200

    can handle priced and not priced customers

--- GCPserver.js
+++ GCPserver.js
@@ -15,14 +15,23 @@ app.use( bodyParser.json() )
 //file handler middle-ware
 const fileHandler = require("fs")
 
-//importing local json file
+//importing local json files
 const testFile = require('./test.json')
+const testFile2 = require('./test2.json')
 
 //CRUD request come below to handle all requests from the frontend 
 
 //handle GET request
 app.get('/customer-list', (req, res) => { 
     fileHandler.readFile('./test.json', (err, content) => {
+        if(err) res.send({"message":"file not found"})
+        else res.send(`${content}`)
+    })
+
+})
+
+app.get('/paid-customer-list', (req, res) => { 
+    fileHandler.readFile('./test2.json', (err, content) => {
         if(err) res.send({"message":"file not found"})
         else res.send(`${content}`)
     })
@@ -43,10 +52,38 @@ app.post('/customer-enlist', (req, res) =>{
     testFile.push(newCustomer)
     fileHandler.writeFile('./test.json', `${JSON.stringify(testFile, null, 2)}`,(err) =>{
         if(err) throw err
-        else res.send({"message":"ungalinda ke ngoku bazofika, usemgceni!"})
+        else res.send({"message":"ungalinda ke ngoku, bazofika bayeza! ufolile!"})
     })
  })
 
+ app.post('/paid-customer-enlist', (req, res) =>{ 
+    
+    let pricedCustomer = {
+        "ilali": req.body.ilali, 
+        "emanini": req.body.emanini, 
+        "igama": req.body.igama, 
+        "umnxeba": req.body.umnxeba,
+        "usuku": req.body.usuku,
+        "ixabiso": req.body.ixabiso
+    }
+
+    testFile2.push(pricedCustomer)
+    fileHandler.writeFile('./test2.json', `${JSON.stringify(testFile2, null, 2)}`,(err) =>{
+        if(err) throw err
+        else res.send({"message":"ihambile, uzochetyelwa!"})
+    })
+ })
+
+
+ app.delete('/delete-customer', (req, res) => {
+    let index = req.body.delete
+    testFile.splice(index, 1)
+    fileHandler.writeFile('./test.json', `${JSON.stringify(testFile, null, 2)}`,(err) =>{
+        if(err) throw err
+        else res.send({"message":"le icustomer isusiwe kwabangekanikwa maxabiso, refresh the page"})
+    })
+
+ })
 
 //Sorting the listening port to be flexible also on deployment.
 const PORT = process.env.PORT || 3001
